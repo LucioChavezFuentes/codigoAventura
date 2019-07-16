d526f61c70e4f5bc47781e28a45b71dd575f9591
Lucio Chavez <luciobertinchavez@gmail.com>
2019-07-16 11:52:08 -0500

    Header Dynamic Behavior to Path

--- src/modules/app/components/TransitionGroupNonAuthUser/Pages/pagesComponents/Header/Header.tsx
+++ src/modules/app/components/TransitionGroupNonAuthUser/Pages/pagesComponents/Header/Header.tsx
@@ -4,36 +4,66 @@ import * as ROUTES from '../../../constants/routes'
 
 import { NavLink, withRouter } from 'react-router-dom';
 
-
-
-
-
-const Header = () => {
-
-   
-  
-  return (
-    <div className="header">
-      <div className="leftSection"> 
-        <NavLink to={ROUTES.LANDING}> Home </NavLink>
-  {/* Check these Links for further implementation 
-  <NavLink to='/about' > About </NavLink>
-        <NavLink to='/contact'> Contact </NavLink>*/}
-        
-      </div>
-
-      <div className="rightSection">
-        <NavLink to='/login'> Log in </NavLink>
-        <NavLink to='/register' > Sign Up </NavLink>
-      </div>
-      
-        
-            
-          
-          
-    </div>
-  )
+interface Props {
+  history: any
+  location: any
 }
+
+
+
+const HeaderBase: React.FC<Props> = ({ location}) => {
+
+  if(location.pathname === ROUTES.LANDING) {
+
+    return (
+
+      <div className="header">
+        <div className="leftSection"> 
+          <NavLink to={ROUTES.LANDING}> Home </NavLink>
+    {/* Check these Links for further implementation 
+    <NavLink to='/about' > About </NavLink>
+          <NavLink to='/contact'> Contact </NavLink>*/}
+          
+        </div>
+  
+        <div className="rightSection">
+          <NavLink to='/login'> Log in </NavLink>
+          
+        </div>
+      </div>
+          
+          )
+  
+  
+} else if (location.pathname === ROUTES.SIGN_UP ) {
+    
+    return(<div className="header">
+              <div className="leftSection"> 
+                <NavLink to={ROUTES.LANDING}> Home </NavLink>
+              </div>
+
+                <div className="rightSection">
+                  <NavLink to='/login'> Log in </NavLink>
+                </div>
+              </div>)
+    
+  } else  {
+
+    return(<div className="header">
+              <div className="leftSection"> 
+                <NavLink to={ROUTES.LANDING}> Home </NavLink>
+              </div>
+
+                <div className="rightSection">
+                <NavLink to='/register' > Sign Up </NavLink>
+                </div>
+              </div>)
+  }
+    
+ 
+}
+
+const Header = withRouter(HeaderBase);
 
 export default Header;
 
