3e69c7d858a43c3b1ce49a9aaa047b62a2f79ec3
Lucio Chavez <luciobertinchavez@gmail.com>
2019-07-29 13:47:23 -0500

    Attempt to Implement the Console Log Embedding

--- src/modules/app/components/RouterGroupAuthUser/Pages/HomePage/HomePage.tsx
+++ src/modules/app/components/RouterGroupAuthUser/Pages/HomePage/HomePage.tsx
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HeaderUser from '../utilsComponents/HeaderUser/HeaderUser'
 
 import brace from 'brace';
 import AceEditor from 'react-ace';
 
-import 'brace/mode/java';
-import 'brace/theme/github';
+import 'brace/mode/javascript';
+import 'brace/theme/terminal';
 
-function onChange(newValue: string) {
-    console.log('change', newValue); 
-  }
+
+
+
 
 const HomePage: React.FC = () => {
+
+    const [code, setCode] = useState< string>('');
+    const [logValue, setLogValue] = useState('');
+
+    function handleChange(newValue: string) {
+      setCode(newValue); 
+    }
+
+    function handleClick() {
+      eval(code)
+    }
+
+    (function(){
+      var oldLog = console.log;
+      console.log = function (message: string) {
+          setLogValue(message)
+          //@ts-ignore
+          oldLog.apply(console, arguments);
+      };
+  })();
+
+
+
     return(
-        <div>
-            <HeaderUser/> 
-            <p>Prográmale aquí chavo</p>
+      <div>
+        <HeaderUser />
 
-            <AceEditor
-    mode="java"
-    theme="github"
-    onChange={onChange}
-    name="UNIQUE_ID_OF_DIV"
-    editorProps={{$blockScrolling: true}}
-  />
-
-
-
+        <div className= 'titleClass'>
+          <p>Prográmale aquí chavo</p>
         </div>
+
+        <div className= 'codeEditor'>
+          <AceEditor
+          mode="javascript"
+          theme="terminal"
+          value={code}
+          onChange={handleChange}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+          />
+        </div>
+
+        <div className='codeOutput'>
+          {logValue}
+        </div>
+
+
+        <button onClick={handleClick}>
+          Correr
+        </button>
+
+        
+
+
+
+      </div>
         
     )
 }
